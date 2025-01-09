3c0447027044e3cbb7b15812eeec271e73fd8a78
non-oft <beingoftenon@gmail.com>
2025-01-08 21:45:44 -0500

    added arrow key movement with variable acceleration and speed

--- scripts/player_ship.gd
+++ scripts/player_ship.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var speed:float = 300.0
+@export var acceleration:float = 20
 
 
 func _physics_process(_delta: float) -> void:
@@ -10,11 +10,17 @@ func _physics_process(_delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var direction_x := Input.get_axis("ui_left", "ui_right")
+	if direction_x:
+		velocity.x = move_toward(velocity.x, direction_x * speed, acceleration)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, acceleration)
+
+	var direction_y := Input.get_axis("ui_up", "ui_down")
+	if direction_y:
+		velocity.y = move_toward(velocity.y, direction_y * speed, acceleration)
+	else:
+		velocity.y = move_toward(velocity.y, 0, acceleration)
 
 	move_and_slide()
 
