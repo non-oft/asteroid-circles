ae2c58ef310965e2583a8f0a881e2dc8635dfaee
non-oft <beingoftenon@gmail.com>
2025-01-08 23:45:28 -0500

    circle spawning on fire input

--- scripts/player_ship.gd
+++ scripts/player_ship.gd
@@ -3,7 +3,13 @@ extends CharacterBody2D
 
 @export var speed:float = 300.0
 @export var acceleration:float = 20
+@export var circle:PackedScene
+@export var fire_distance:float = 30
+@export var fire_velocity:float = 10
+var my_parent
 
+func _ready() -> void:
+	my_parent = $".."
 
 func _physics_process(_delta: float) -> void:
 	
@@ -30,3 +36,7 @@ func _physics_process(_delta: float) -> void:
 func check_for_fire():
 	if Input.is_action_just_pressed("fire"):
 		print("shot fired")
+		var this_circle=circle.instantiate()
+		my_parent.add_child(this_circle)
+		this_circle.position = self.position + Vector2(0, -fire_distance)
+		this_circle.linear_velocity = Vector2(0, -fire_velocity)
