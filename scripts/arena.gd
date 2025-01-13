bdafa52eb57efcd8498b8501f25803ff295c844c
Tate Wilson <tatekwilson@proton.me>
2025-01-12 22:14:22 -0800

    added a gravity well to the arena

--- scripts/arena.gd
+++ scripts/arena.gd
@@ -4,10 +4,14 @@ extends StaticBody2D
 @export var scale_multiplier: float = 2
 
 var scale_cap
+const GRAVITY:float = 10
+
+var gravity_direction = 1.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	scale_cap = scale * scale_multiplier
+	$GravityButton.button_down.connect(_reverse_gravity)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,3 +21,16 @@ func _process(delta: float) -> void:
 
 		if $Camera2D.zoom.x != 1:
 			$Camera2D.zoom = lerp($Camera2D.zoom,Vector2(1,1), 3 * delta) 
+
+
+func _physics_process(_delta: float) -> void:
+	var circles = get_tree().get_nodes_in_group("circle")
+
+	for circle in circles:
+		var to_center = circle.position - self.position
+		to_center = to_center.normalized() 
+
+		circle.linear_velocity += (to_center * GRAVITY) * gravity_direction
+		
+func _reverse_gravity():
+	gravity_direction *= -1
