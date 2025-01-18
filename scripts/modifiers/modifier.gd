c70bc883f98ab782d2b4421993a2736bf4813b6b
non-oft <beingoftenon@gmail.com>
2025-01-18 01:53:16 -0500

    roughed out apply_score function in score_handler, defined a number of variables in the parent modifier script

--- scripts/modifiers/modifier.gd
+++ scripts/modifiers/modifier.gd
@@ -4,3 +4,59 @@ func calculate(_initial) -> float:
     print("Error: modifier lacks calculate function")
     return 0.0
 #TODO: swap out for proper error?
+
+
+
+#main variables potentially needed for calculating each score increase:
+
+var circle_size
+#resulting size of currently merging circles
+
+var combo
+#current combo
+
+var combo_max
+#max combo achieved this run
+
+var current_score
+#existing score prior to current merge's effects
+
+var merge_position
+#position where merge is happening
+
+var ship_position
+#position where the player's ship currently is
+
+var current_circles: Array
+#array containing all circles that currently exist, with circle size as the index; alternatively maybe this could potentially be a dictionary or something
+
+var full_deck: Array
+#array containing all circles in the player's current deck (including those already fired), in the current order
+
+var deck_index: int
+#index of next circle to be fired within player's current deck
+
+var map_size
+#current size of the map, based on score (to be passed in from elsewhere, not just calculated again here, in case other random nonsense gets factored in somewhere for some reason?)
+
+
+#quality and shininess of modifier, set when modifier is created, to augment numerical values inside it:
+
+var modifier_quality: int
+#quality of modifier, from +0 (functional) to +12 (impossible)
+
+var modifier_shininess: float
+#shininess of modifier, from *.5(corroded) to *10+ (otherworldly)
+
+
+
+#configurable elements to be used in modifiers, to be changed via upgrades in shops or something?:
+
+var modifier_invert: bool
+#configurable boolean to invert the behavior of the modifier
+
+var modifier_look_at
+#configurable indicator for left, right, first, last, etc; TODO replace this comment with a proper reference list and use an int value
+
+var modifier_circle_size
+#configurable circle size to be compared against
--- scripts/score_handler.gd
+++ scripts/score_handler.gd
@@ -4,10 +4,6 @@ extends Node
 var modifiers_array: Array
 
 
-func _ready() -> void:
-	var test_modifier = preload("res://scenes/packed_scenes/modifiers/test_modifier.tscn").instantiate()
-	add_child(test_modifier)
-	modifiers_array = [test_modifier]
 
 var current_score: int:
 	set(value):
@@ -17,11 +13,21 @@ var current_score: int:
 #Signals
 signal points_added(points)
 
+
+func _ready() -> void:
+	var test_modifier = preload("res://scenes/packed_scenes/modifiers/test_modifier.tscn").instantiate()
+	add_child(test_modifier)
+	modifiers_array = [test_modifier]
+	#^all this is temporary, populate the array with modifier nodes properly from wherever
+
+
 func apply_score(_merge_position:Vector2, merge_result_size:int):
 	print("apply_score called with merge_result_size = ", merge_result_size)
+	var score_temp = merge_result_size
 	for modifier in modifiers_array:
-		print("should be merged circle size +1: ",modifier.calculate(merge_result_size))
+		print("should be merged circle size +1: ",modifier.calculate(score_temp))
 
-		current_score += modifier.calculate(merge_result_size)
+		score_temp =  modifier.calculate(score_temp)
+	current_score += score_temp
 	
 
